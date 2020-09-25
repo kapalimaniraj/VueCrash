794140399ac60459e191e1f6e1b04fb5e072cde0
Mani Kapali <kapalimaniraj@gmail.com>
2020-09-25 23:09:17 +0545

    Updates made New .view file created

---
+++ hello-world/src/HelloWorld.vue
@@ -0,0 +1,26 @@
+
+<!---->
+<template>
+<h1>Hello World</h1>
+<p>
+    My Name is {{myName}} and I work at {{company}}
+</p>
+</template>
+
+<!-- Logic -->
+<script>
+export default{
+    data() {
+        return{
+            myName: "Mani Kapali",
+            company: "Delight Digital"
+        }
+    }
+}
+</script>
+
+
+
+<!-- Style -->
+<style scoped>
+</style>
